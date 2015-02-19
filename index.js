13e1ef2b0fee7670ea387196e7d6b786f6d720e3
Lars van der Schans <larsvanderschans@gmail.com>
2015-02-19 11:40:42 +0100

    Structure

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * angular-server-with-sockets
+ *
+ * Description:  The perfect angular server with sockets
+ *-----------------------------------------------------
+ * Author: Lars van der Schans
+ * Email:  lars@wodanbrothers.com
+ *-----------------------------------------------------
+ */

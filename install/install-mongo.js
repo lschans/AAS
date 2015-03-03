a2c6d6618303cd981b91b85fb992bff198be6283
Lars van der Schans <larsvanderschans@gmail.com>
2015-03-03 18:21:00 +0100

    Finally fixed sessions

--- install/install-mongo.js
+++ install/install-mongo.js
@@ -1,5 +1,30 @@
-var Server = require("mongo-sync").Server;
+var server = require("mongo-sync").Server;
 var server = new Server('127.0.0.1');
-var result = server.db("test").getCollection("posts").find().toArray();
-console.log(result);
+
+var collections = server.db("angularServer");
+//var collections = server.db('*').collectionNames();
+
+/*
+ db.sample_coll.update( { _id:"Joe" }, { $set: { info1: "abc", info2:
+ "def" }, $inc: { x: 1 } }, true );
+ - and -
+ db.sample_coll.update( { _id:"Joe" }, { $set: { info1: "abc", info2:
+ "def" }, $push: { item_list: { data1: "aaa", data2: "bbb" } } },
+ true );
+ */
+
+//var sessions = server.db("angularServer");
+
+if(collections.collectionNames().length > 0) {
+    // Table exists
+    console.log('Database is already set up, we are fine');
+} else {
+    // Table doesn't exist
+    collections.getCollection("sessions").update({ sessionID:"dummy" }, { $set: { sessionID: "dummy", ip: "0.0.0.0" }}, {upsert:true});
+
+}
+
+var upserted = collections.getCollection("sessions").update({ sessionID:"dummy" }, { $set: { sessionID: "dummy", ip: "0.0.0.0" }}, {upsert:true});
+
+console.log(upserted);
 server.close();
--- install/install.sh
+++ install/install.sh
@@ -4,9 +4,8 @@ CN=$(npm list -g --depth=0 common-node | grep common-node | cut -d "@" -f 2)
 
 if [ $CN > 0 ];
 then
-    echo "CN is installed";
     common-node install-mongo.js
 else
-echo "Can't continue common-node is not installed";
+echo "Installing common-node.";
     exit 0
 fi;

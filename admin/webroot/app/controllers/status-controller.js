a91ee955ab3fec27949607cd7f169ef51808d27b
Lars van der Schans <larsvanderschans@gmail.com>
2015-03-09 09:10:16 +0100

    ...

--- admin/webroot/app/controllers/status-controller.js
+++ admin/webroot/app/controllers/status-controller.js
@@ -2,13 +2,33 @@ aasAdmin.controller('statusController', ["$scope", "$cookies", "ioSocket", funct
     var sessionID = $cookies['X-Session-ID'];
     var sessionString = 'SESSION ID: ' + sessionID;
 
+    if(typeof($scope.windows) == 'undefined') {
+        $scope.windows = [
+            {name: 'Session', content: sessionString}
+        ];
+        ioSocket.emit('admin', {sessionID:sessionID, command:'config', subcommand:'global'});
+        ioSocket.emit('admin', {sessionID:sessionID, command:'config', subcommand:'users'});
+        ioSocket.emit('admin', {sessionID:sessionID, command:'config', subcommand:'session'});
+        ioSocket.emit('admin', {sessionID:sessionID, command:'config', subcommand:'ssl'});
+    };
+
     ioSocket.on('connect', function () {
         console.log('connected');
     });
 
-    $scope.windows = [
-        {name:'Session', content:sessionString},
-        {name:'Test 1', content:'123'}
-    ];
+    $scope.$on('socket:admin', function(event, message) {
+        console.log('incoming message');
+        if (!message) {
+            console.log('invalid message', 'event', event,
+                'data', JSON.stringify(message));
+            return;
+        }
+
+        $scope.$apply(function() {
+            $scope.windows.push({name:message.subcommand, content:JSON.stringify(message.data,null,"    ")});
+        });
+    });
+
+
 }]);
 

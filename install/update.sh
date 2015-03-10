7e3df5d6c552eea67e0995e35f982dfba328047b
Lars van der Schans <larsvanderschans@gmail.com>
2015-03-10 13:24:36 +0100

    update script

--- install/update.sh
+++ install/update.sh
@@ -1,1 +1,24 @@
 #!/bin/sh
+
+appdir=$(dirname $(pwd))
+
+cd $appdir
+npm install
+
+cd $appdir/node_modules/\@dyflexis/client-info/node_modules/
+npm install
+cd $appdir/node_modules/\@dyflexis/parse-request/node_modules/
+npm install
+cd $appdir/node_modules/\@dyflexis/spdy-server/node_modules/
+npm install
+cd $appdir/node_modules/\@dyflexis/serve/node_modules/
+npm install
+cd $appdir/node_modules/\@dyflexis/session/node_modules/
+npm install
+
+cd $appdir/admin/public
+bower install
+
+cd $appdir/admin/webroot
+bower install
+
